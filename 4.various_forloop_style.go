d29909bbbd10c748c4d67c34d74aa8b9d48c4b5f
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-01 10:55:24 +0800

    add Range style forloop & rune to string

--- 4.various_forloop_style.go
+++ 4.various_forloop_style.go
@@ -2,6 +2,8 @@ package main
 
 import(
     "fmt"
+    //"reflect"
+    //"strconv"
 )
 
 func main(){
@@ -28,6 +30,16 @@ func main(){
         if j>=10{
             break
         }
+    }
+    fmt.Println();
+
+    //Range style
+    var arrString string = "This is a string"    
+    for _, c := range arrString[0:]{
+        // fmt.Println(reflect.TypeOf(c)) == int32
+        // c 的型別是 rune，相當於 C++ 中的 char，直接 print 會當作 int32 印出數字
+        // string(c) 才會看到對應的字元
+        fmt.Print(string(c))
     }
     fmt.Println();
 }
