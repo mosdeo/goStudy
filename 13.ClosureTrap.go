a282954f4c7446d021518a30d6d6e499f937a2df
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-06 13:58:54 +0800

    Add Clousre Trap

---
+++ 13.ClosureTrap.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var numbers []int = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	var refToPrintInt []func() = nil
+	for _, num := range numbers {
+
+		// 寫一個匿名函式
+		refToPrintInt = append(refToPrintInt, func() {
+			fmt.Println(num)
+		})
+		refToPrintInt[len(refToPrintInt)-1]()
+	}
+
+	fmt.Println(reflect.TypeOf(refToPrintInt))
+
+	for _, f := range refToPrintInt {
+		f()
+	}
+
+	// 多年前寫 C# 踩過的坑，我這邊再重現一遍
+	// 這個例子說明閉包（Closure）會吃進外部變數的位址(address)，而非值(value)。
+	// 這有點像是 LINQ 只是先建立「預約命令」，特定指令才會強制執行。
+}
