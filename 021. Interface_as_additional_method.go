75d5c41a1134cada28483f4b0e078f63e14f4a91
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-13 13:04:41 +0800

    之前錯誤，沒有實作到 interface 只是實作同名稱函式。這個才是實作函數真正受 interface 約束的版本

--- 021. Interface_as_additional_method.go
+++ 021. Interface_as_additional_method.go
@@ -12,7 +12,15 @@ import (
 type BodyWeightChange interface {
 	Eat(anyFood string) error
 	Excretion() error
-	Test() //沒有實現也不會發生錯誤
+	//Test() //沒有實現會發生錯誤
+}
+
+func DoEat(someone BodyWeightChange, anyFood string) error {
+	return someone.Eat(anyFood)
+}
+
+func DoExcretion(someone BodyWeightChange) error {
+	return someone.Excretion()
 }
 
 // 這是「貓」結構
@@ -28,7 +36,7 @@ type People struct {
 }
 
 //「貓」實現改變體重的 Interface
-func (c *Cat) Eat(catFood string) error {
+func (c Cat) Eat(catFood string) error {
 	if 10 <= c.weight {
 		return fmt.Errorf("已經 %d kg，太胖了不可以再吃", c.weight)
 	}
@@ -37,7 +45,7 @@ func (c *Cat) Eat(catFood string) error {
 	return nil
 }
 
-func (c *Cat) Excretion() error {
+func (c Cat) Excretion() error {
 	if 1 >= c.weight {
 		return fmt.Errorf("已經 %d kg，太瘦了不可以再拉", c.weight)
 	}
@@ -47,19 +55,19 @@ func (c *Cat) Excretion() error {
 }
 
 //「人」實現改變體重的 Interface
-func (p *People) Eat() error {
+func (p People) Eat(peopleFood string) error {
 	if 99 <= p.weight {
 		return fmt.Errorf("已經 %d kg，太胖了不可以再吃", p.weight)
 		//這裡若用 fmt.Sprintf() 會被編輯器提醒應該改用 fmt.Errorf()，但硬要用也可以跑。
 		//等等來看看有什麼差別
 		//懂了，原來是 errors.New(fmt.Sprintf()) == fmt.Errorf()
 	}
-	fmt.Printf("%s Eat() 吃了便當\n", p.name)
+	fmt.Printf("%s Eat() 吃了一餐, 裡面有:%s\n", p.name, peopleFood)
 	p.weight++
 	return nil
 }
 
-func (p *People) Excretion() error {
+func (p People) Excretion() error {
 	if 1 >= p.weight {
 		return fmt.Errorf("已經 %d kg，太瘦了不可以再上廁所", p.weight)
 	}
@@ -76,30 +84,30 @@ func main() {
 	fmt.Printf("I'm %s, my body weight is %d\n", firstPeople.name, firstPeople.weight)
 
 	fmt.Println("\n========= 執行體重增加介面 =========")
-	firstCat.Eat("肉")
-	firstPeople.Eat()
+	DoEat(&firstPeople, "肉")
+	DoEat(&firstCat, "肉")
 	fmt.Printf("I'm %s, my body weight is %d\n", firstCat.name, firstCat.weight)
 	fmt.Printf("I'm %s, my body weight is %d\n", firstPeople.name, firstPeople.weight)
 
 	fmt.Println("\n========= 執行體重減少介面 =========")
-	firstCat.Excretion()
-	firstCat.Excretion()
-	firstPeople.Excretion()
-	firstPeople.Excretion()
+	DoExcretion(firstCat)
+	DoExcretion(firstCat)
+	DoExcretion(firstPeople)
+	DoExcretion(firstPeople)
 	fmt.Printf("I'm %s, my body weight is %d\n", firstCat.name, firstCat.weight)
 	fmt.Printf("I'm %s, my body weight is %d\n", firstPeople.name, firstPeople.weight)
 
-	fmt.Println("\n========= 在超過範圍的邊緣瘋狂試探 =========")
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstCat.Excretion())
-	fmt.Println(firstPeople.Eat())
-	fmt.Println(firstPeople.Eat())
-	fmt.Println(firstPeople.Eat())
-	fmt.Println(firstPeople.Eat())
-	fmt.Println(firstPeople.Eat())
+	fmt.Println("\n========= 在超過正常體重範圍的邊緣瘋狂試探 =========")
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoExcretion(firstCat))
+	fmt.Println(DoEat(firstPeople, "肉"))
+	fmt.Println(DoEat(firstPeople, "肉"))
+	fmt.Println(DoEat(firstPeople, "肉"))
+	fmt.Println(DoEat(firstPeople, "肉"))
+	fmt.Println(DoEat(firstPeople, "肉"))
 }
