84d1e5b121dd96190eb098bf7bc9907f3b392344
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-16 15:11:11 +0800

    Add RaceCondition demo

---
+++ 031. Goroutine_RaceCondition.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 999; i++ {
+		go RaceCondition()
+	}
+}
+
+func TrueRandom(max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max)
+}
+
+func RaceCondition() {
+	//	模擬應用中的計算延遲
+	var DoSomethingCalc = func() {
+		time.After(time.Millisecond * time.Duration(TrueRandom(99)))
+	}
+
+	var data int
+	go func() {
+		data++
+	}()
+
+	if 0 == data {
+		DoSomethingCalc()
+		fmt.Printf("%d", data)
+	}
+}
