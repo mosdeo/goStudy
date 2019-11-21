866353335644ead86ccd694fad50e02f91f603c9
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-21 17:01:04 +0800

    窮舉 test case 並計算每個 case 的時間

--- myLeetCode/Concurrency_Print in Order.go
+++ myLeetCode/Concurrency_Print in Order.go
@@ -2,49 +2,84 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
+	"reflect"
+	"runtime"
 	"sync"
+	"time"
 )
 
-func main() {
-	var First = func(wg *sync.WaitGroup) {
-		fmt.Printf("First")
-		wg.Done()
+func First(wg *sync.WaitGroup, streamSync [3]chan interface{}) {
+	fmt.Printf("First")
+	streamSync[0] <- nil
+	wg.Done()
+}
+
+func Second(wg *sync.WaitGroup, streamSync [3]chan interface{}) {
+	<-streamSync[0]
+	fmt.Printf("Second")
+	streamSync[1] <- nil
+	wg.Done()
+}
+
+func Third(wg *sync.WaitGroup, streamSync [3]chan interface{}) {
+	<-streamSync[1]
+	fmt.Printf("Third")
+	wg.Done()
+}
+
+func GetValueName(v interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(v).Pointer()).Name()
+}
+
+func PrintInOrde(callOrder [3]int) {
+	// 取得輸入順序
+	// var inputCallOrder []int
+	// for _, arg := range os.Args[1:] {
+	// 	num, _ := strconv.Atoi(arg)
+	// 	inputCallOrder = append(inputCallOrder, num)
+	// }
+	inputCallOrder := callOrder
+	fmt.Println("[]inputCallOrder:", inputCallOrder)
+
+	// make an array of unbuffered
+	var streamSync [3]chan interface{}
+	for i := range streamSync {
+		streamSync[i] = make(chan interface{})
 	}
 
-	var Second = func(wg *sync.WaitGroup) {
-		fmt.Printf("Second")
-		wg.Done()
-	}
-
-	var Third = func(wg *sync.WaitGroup) {
-		fmt.Printf("Third")
-		wg.Done()
-	}
-
-	wg := &sync.WaitGroup{}
-	var functionNumTable = map[int]func(*sync.WaitGroup){
+	// 建立 [int:func] 對應表
+	var functionNumTable = map[int]func(*sync.WaitGroup, [3]chan interface{}){
 		1: First,
 		2: Second,
 		3: Third,
 	}
 
-	// 取得輸入順序
-	var inputCallOrder []int
-	for _, arg := range os.Args[1:] {
-		num, _ := strconv.Atoi(arg)
-		inputCallOrder = append(inputCallOrder, num)
-	}
-	fmt.Println("inputCallOrder:", inputCallOrder)
-
 	//依照輸入順序呼叫 goroutine
-
+	wg := &sync.WaitGroup{}
 	for _, fNum := range inputCallOrder {
+		fmt.Println("Call:", GetValueName(functionNumTable[fNum]))
 		wg.Add(1)
-		go functionNumTable[fNum](wg)
+		go functionNumTable[fNum](wg, streamSync)
 	}
 
 	wg.Wait()
 	fmt.Println()
+}
+
+func main() {
+	var testCases = [][3]int{
+		{1, 2, 3},
+		{1, 3, 2},
+		{2, 1, 3},
+		{2, 3, 1},
+		{3, 1, 2},
+		{3, 2, 1},
+	}
+
+	for _, theCase := range testCases {
+		startTime := time.Now()
+		PrintInOrde(theCase)
+		endTime := time.Now()
+		fmt.Printf("Case %v Spent time:%v\n", theCase, endTime.Sub(startTime))
+	}
 }
