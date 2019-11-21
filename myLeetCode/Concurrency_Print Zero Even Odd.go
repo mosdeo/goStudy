71e44c54ee36a77e738f3abe9abe6226076a8113
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-21 19:48:59 +0800

    已經可以正確輸出，但最後不知道 deadlock 在哪裏

--- myLeetCode/Concurrency_Print Zero Even Odd.go
+++ myLeetCode/Concurrency_Print Zero Even Odd.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"sync"
 )
 
 type ZeroEvenOdd struct {
-	n  int
-	wg *sync.WaitGroup
+	n                int
+	wg               *sync.WaitGroup
+	streamEvenToZero chan interface{}
+	streamOddToZero  chan interface{}
+	streamZeroToEven chan interface{}
+	streamZeroToOdd  chan interface{}
+	streamBreakFlag  chan interface{}
+	breakFlag        bool
 }
-
-// func(this *ZeroEvenOdd) SetN(n int){
-// 	this.n = n
-// }
 
 func (this *ZeroEvenOdd) SetWaitGroup(wg *sync.WaitGroup) {
 	this.wg = wg
@@ -21,16 +25,61 @@ func (this *ZeroEvenOdd) SetWaitGroup(wg *sync.WaitGroup) {
 func (this *ZeroEvenOdd) Zero(printNumber func(int)) {
 	defer this.wg.Done()
 
+	for i := 0; i < this.n; i++ {
+		select {
+		case <-this.streamOddToZero:
+			printNumber(0)
+			this.streamZeroToEven <- nil
+		case <-this.streamEvenToZero:
+			printNumber(0)
+			this.streamZeroToOdd <- nil
+		case <-this.streamBreakFlag:
+			fmt.Println("Zero() Done")
+			goto RETURN
+		default:
+			i--
+			// 	if this.breakFlag {
+			// 		break
+			// 	}
+		}
+	}
+RETURN:
 }
 
 func (this *ZeroEvenOdd) Even(printNumber func(int)) {
-	defer this.wg.Done()
+	// defer func() { this.breakFlag = true }() // 通知其他兩個 goroutine 結束
 
+	for i := 1; i <= this.n; i += 2 {
+		<-this.streamZeroToEven
+		if this.breakFlag {
+			break
+		}
+		printNumber(i)
+		this.streamEvenToZero <- nil
+	}
+
+	this.streamBreakFlag <- nil
+	this.streamBreakFlag <- nil
+	fmt.Println("Even() Done")
+	this.wg.Done()
 }
 
 func (this *ZeroEvenOdd) Odd(printNumber func(int)) {
-	defer this.wg.Done()
+	// defer func() { this.breakFlag = true }() // 通知其他兩個 goroutine 結束
 
+	for i := 2; i <= this.n; i += 2 {
+		<-this.streamZeroToOdd
+		if this.breakFlag {
+			break
+		}
+		printNumber(i)
+		this.streamOddToZero <- nil
+	}
+
+	this.streamBreakFlag <- nil
+	this.streamBreakFlag <- nil
+	fmt.Println("Odd() Done")
+	this.wg.Done()
 }
 
 func PrintNumber(x int) {
@@ -38,15 +87,26 @@ func PrintNumber(x int) {
 }
 
 func main() {
-	var zeo = &ZeroEvenOdd{n: 5}
+	testNum, _ := strconv.Atoi(os.Args[1])
+	var zeo = &ZeroEvenOdd{
+		n:                testNum,
+		streamEvenToZero: make(chan interface{}),
+		streamOddToZero:  make(chan interface{}),
+		streamZeroToEven: make(chan interface{}),
+		streamZeroToOdd:  make(chan interface{}),
+	}
 
 	//設定同步
 	wg := &sync.WaitGroup{}
 	zeo.SetWaitGroup(wg)
 
 	wg.Add(3)
+	go func() {
+		zeo.streamOddToZero <- nil
+	}()
 	go zeo.Zero(PrintNumber)
 	go zeo.Even(PrintNumber)
 	go zeo.Odd(PrintNumber)
 	wg.Wait()
+	fmt.Println()
 }
