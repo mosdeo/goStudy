6563a53526bc31780c3b3aad5fe67db1e74f18d9
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-11-23 15:05:52 +0800

    Add solution in LeetCode Concurrency「Print FooBar」

--- myLeetCode/Concurrency_Print FooBar Alternately.go
+++ myLeetCode/Concurrency_Print FooBar Alternately.go
@@ -20,21 +20,25 @@ type FooBar struct {
 }
 
 func (this *FooBar) Foo(printFoo func()) {
-	for i := 0; i < this.n; i++ {
 
+	for i := 0; i < this.n; {
 		// printFoo() outputs "foo". Do not change or remove this line.
 		<-this.streamBarToFoo
 		printFoo()
+		i++
 		this.streamFooToBar <- struct{}{}
 	}
+
+	<-this.streamBarToFoo //等待 Bar() 印完最後一次
 }
 
 func (this *FooBar) Bar(printBar func()) {
-	for i := 0; i < this.n; i++ {
 
+	for i := 0; i < this.n; {
 		// printBar() outputs "bar". Do not change or remove this line.
 		<-this.streamFooToBar
 		printBar()
+		i++
 		this.streamBarToFoo <- struct{}{}
 	}
 
@@ -42,20 +46,27 @@ func (this *FooBar) Bar(printBar func()) {
 }
 
 func main() {
-	fmt.Println()
 
-	fooBar := &FooBar{
-		n:              4,
-		streamFooToBar: make(chan struct{}),
-		streamBarToFoo: make(chan struct{}),
-		streamEnd:      make(chan struct{}),
+	var PrintFooBar = func(times int) {
+
+		fooBar := &FooBar{
+			n:              times,
+			streamFooToBar: make(chan struct{}),
+			streamBarToFoo: make(chan struct{}),
+			streamEnd:      make(chan struct{}),
+		}
+
+		go fooBar.Foo(func() { fmt.Printf("Foo") })
+		go fooBar.Bar(func() { fmt.Printf("Bar ") })
+		fooBar.streamBarToFoo <- struct{}{} //啟動
+		<-fooBar.streamEnd                  //as wg.Wait()
+		fmt.Println()
 	}
 
-	go fooBar.Foo(func() { fmt.Printf("Foo") })
-	go fooBar.Bar(func() { fmt.Printf("Bar ") })
-	fooBar.streamBarToFoo <- struct{}{} //啟動
-	<-fooBar.streamEnd                  //as wg.Wait()
+	testCase := []int{0, 1, 2, 3, 4, 5, 7}
 
-	//關閉各個 worker
-
+	for _, repeat := range testCase {
+		fmt.Printf("Repeat %d: ", repeat)
+		PrintFooBar(repeat)
+	}
 }
