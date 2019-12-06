bd7c6e9fd679ff6a684e6a4bce64f26574d00adb
Lin, Kao-Yuan <mosdeo@gmail.com>
2019-12-06 15:02:53 +0800

    解完從 MySQL 讀取時間的坑

--- 044. DataBaseSQL.go
+++ 044. DataBaseSQL.go
@@ -3,30 +3,40 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"reflect"
+	"time"
 
 	_ "github.com/Go-SQL-Driver/MySQL"
 )
 
+func TrueRandom(max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max)
+}
+
 func main() {
-	db, err := sql.Open("mysql", "astaxie:astaxie@/test?charset=utf8")
+	//db, err := sql.Open("mysql", "user:password@/dbname")
+	db, err := sql.Open("mysql", "root:password@/sreetchat?charset=utf8mb4")
 	checkErr(err)
 
 	//插入資料
-	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	stmt, err := db.Prepare("INSERT users SET username=?,age=?,sex=?,timestamp=?")
 	checkErr(err)
 
-	res, err := stmt.Exec("astaxie", "研發部門", "2012-12-09")
+	// 執行 query 或 prepared
+	res, err := stmt.Exec("LKY", TrueRandom(100), "M", time.Now())
 	checkErr(err)
 
 	id, err := res.LastInsertId()
+	fmt.Println("LastInsertId():", id)
 	checkErr(err)
 
-	fmt.Println(id)
 	//更新資料
-	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	stmt, err = db.Prepare("update users set username=? where id=?")
 	checkErr(err)
 
-	res, err = stmt.Exec("astiXXXXXupdate", id)
+	res, err = stmt.Exec("LKY_New", id)
 	checkErr(err)
 
 	affect, err := res.RowsAffected()
@@ -35,21 +45,39 @@ func main() {
 	fmt.Println(affect)
 
 	//查詢資料
-	rows, err := db.Query("SELECT * FROM userinfo")
+	rows, err := db.Query("SELECT * FROM sreetchat.users")
 	checkErr(err)
 
 	for rows.Next() {
-		var uid int
+		var id int
 		var username string
-		var department string
-		var created string
-		err = rows.Scan(&uid, &username, &department, &created)
+		var age string
+		var sex string
+		var iTimeStamp interface{}
+		err = rows.Scan(&id, &username, &age, &sex, &iTimeStamp)
 		checkErr(err)
 
-		fmt.Println(uid)
-		fmt.Println(username)
-		fmt.Println(department)
-		fmt.Println(created)
+		// 如果時間戳記不為空
+		if nil != iTimeStamp {
+			// 判斷時間戳記類型
+			switch v := iTimeStamp.(type) {
+			case []uint8:
+				t, _ := time.Parse("2006-01-02 15:04:05", string(v))
+				fmt.Printf("%v, %v, %v, %v, %v, %v\n", id, username, age, sex, t, reflect.TypeOf(v))
+			case time.Time:
+				fmt.Printf("%v, %v, %v, %v, %v, %v\n", id, username, age, sex, v, reflect.TypeOf(v))
+			case string:
+				//t, err := time.Parse("2006-01-02 15:04:05", v)
+				t, err := time.Parse(time.Now().String(), v)
+				if nil != err {
+					fmt.Printf("%v, %v, %v, %v, %v, %v\n", id, username, age, sex, t, reflect.TypeOf(t))
+				} else {
+					fmt.Println(err)
+				}
+			}
+		} else {
+			fmt.Printf("%v, %v, %v, %v, %v\n", id, username, age, sex, "timestamp is null")
+		}
 	}
 
 	//刪除資料
@@ -58,6 +86,7 @@ func main() {
 
 func checkErr(err error) {
 	if nil != err {
+		fmt.Errorf("%s", err)
 		panic(err)
 	}
 }
