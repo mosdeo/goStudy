f3576d8386ca9b9f28c9dce087801064364a76e2
Lin, Kao-Yuan <mosdeo@gmail.com>
2020-02-22 22:32:19 +0800

    交接棒不指定對象的版本

--- myLeetCode/Concurrency_FizzBuzzMulitThreaded.go
+++ myLeetCode/Concurrency_FizzBuzzMulitThreaded.go
@@ -1,48 +1,89 @@
-// 改為「不採用 channel，讓各個 goroutine 自行負擔檢查整除條件的責任」的版本。
-// 不過這個版本好像太簡單了，下一個版本要用各自邊跑邊交棒的。
+// 交接棒不指定對象的版本
 
 package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
 
 type FizzBuzz struct {
-	n  int
-	wg *sync.WaitGroup
+	n           int
+	wg          *sync.WaitGroup
+	streamBaton chan int
 }
 
 func (this *FizzBuzz) PrintFizz() {
 	defer this.wg.Done()
 
-	if 0 == this.n%3 && 0 != this.n%5 {
-		fmt.Print("Fizz, ")
+	for i := 0; i <= this.n; i++ {
+		if (0 == i%3) && (0 != i%5) {
+			nextNum := <-this.streamBaton //接棒
+			if i == nextNum {
+				fmt.Printf("Fizz(%d), ", i)
+				this.streamBaton <- i + 1 //交棒
+			} else {
+				this.streamBaton <- nextNum //把數字還回去
+				i--
+			}
+			runtime.Gosched()
+		}
 	}
 }
 
 func (this *FizzBuzz) PrintBuzz() {
 	defer this.wg.Done()
 
-	if 0 != this.n%3 && 0 == this.n%5 {
-		fmt.Print("Buzz, ")
+	for i := 0; i <= this.n; i++ {
+		if (0 != i%3) && (0 == i%5) {
+			nextNum := <-this.streamBaton //接棒
+			if i == nextNum {
+				fmt.Printf("Buzz(%d), ", i)
+				this.streamBaton <- i + 1 //交棒
+			} else {
+				this.streamBaton <- nextNum //把數字還回去
+				i--
+			}
+			runtime.Gosched()
+		}
 	}
 }
 
 func (this *FizzBuzz) PrintFizzBuzz() {
 	defer this.wg.Done()
 
-	if 0 == this.n%(3*5) {
-		fmt.Print("FizzBuzz, ")
+	for i := 0; i <= this.n; i++ {
+		if 0 == i%(3*5) {
+			nextNum := <-this.streamBaton //接棒
+			if i == nextNum {
+				fmt.Printf("FizzBuzz(%d), ", i)
+				this.streamBaton <- i + 1 //交棒
+			} else {
+				this.streamBaton <- nextNum //把數字還回去
+				i--
+			}
+			runtime.Gosched()
+		}
 	}
 }
 
 func (this *FizzBuzz) PrintNumber() {
 	defer this.wg.Done()
 
-	if 0 != this.n%3 && 0 != this.n%5 {
-		fmt.Printf("%d, ", this.n)
+	for i := 0; i <= this.n; i++ {
+		if (0 != i%3) && (0 != i%5) {
+			nextNum := <-this.streamBaton //接棒
+			if i == nextNum {
+				fmt.Printf("%d, ", i)
+				this.streamBaton <- i + 1 //交棒
+			} else {
+				this.streamBaton <- nextNum //把數字還回去
+				i--
+			}
+			runtime.Gosched()
+		}
 	}
 }
 
@@ -51,19 +92,23 @@ func main() {
 	start := time.Now()
 
 	fizzbuzz := &FizzBuzz{
-		wg: &sync.WaitGroup{},
+		wg:          &sync.WaitGroup{},
+		streamBaton: make(chan int, 1),
 	}
 
-	for testCase := 0; testCase <= 19; testCase++ {
-		for i := 0; i <= testCase; i++ {
-			fizzbuzz.n = i
-			fizzbuzz.wg.Add(4)
-			go fizzbuzz.PrintFizz()
-			go fizzbuzz.PrintBuzz()
-			go fizzbuzz.PrintFizzBuzz()
-			go fizzbuzz.PrintNumber()
-			fizzbuzz.wg.Wait()
-		}
+	for testCase := 0; testCase <= 20; testCase++ {
+		fizzbuzz.n = testCase
+
+		fizzbuzz.wg.Add(4)
+		go fizzbuzz.PrintFizz()
+		go fizzbuzz.PrintBuzz()
+		go fizzbuzz.PrintFizzBuzz()
+		go fizzbuzz.PrintNumber()
+
+		fizzbuzz.streamBaton <- 0 //啟動交棒
+		fizzbuzz.wg.Wait()
+		<-fizzbuzz.streamBaton
+
 		fmt.Println() //這個 Test Case 結束了，換行。
 	}
 
