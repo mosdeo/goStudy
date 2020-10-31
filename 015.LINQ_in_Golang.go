e49009193811a66b5c062870bcf22167902f2cde
Lin, Kao-Yuan <mosdeo@gmail.com>
2020-10-31 09:37:33 +0800

    修正脫褲子放屁的用法

--- 015.LINQ_in_Golang.go
+++ 015.LINQ_in_Golang.go
@@ -5,46 +5,42 @@ import (
 	"strings"
 
 	"github.com/ahmetb/go-linq"
-	. "github.com/ahmetb/go-linq"
 )
-
-func LinqQueryToSliceInt(linqQuery linq.Query) (outSlice []int) {
-	linqQuery.ToSlice(&outSlice)
-	return outSlice
-}
 
 func main() {
 	var numbers []int = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 	var words []string = []string{"Cat", "Dog", "People", "Cup", "Apple", "Boss", "Zoo", "X-ray", "Github"}
 
 	// LINQ 基本用法
-	fmt.Println(LinqQueryToSliceInt(From(numbers).Reverse())) //反轉
-	fmt.Println(LinqQueryToSliceInt(Range(99, 3)))            //產生連續數列
-	fmt.Println(LinqQueryToSliceInt(Range(100, -3)))          //產生連續數列
-	fmt.Println(From(numbers).SumInts())                      //加總
-	fmt.Println(From(numbers).Average())                      //平均
-	fmt.Println(From(numbers).Min(), From(numbers).Max())     //最大最小
-	fmt.Println(From(words).Last())                           //取最後一個
+	fmt.Println(linq.Range(99, 3).Results()) //產生連續數列
+	// (下面這條有問題，要修改)
+	fmt.Println(linq.Range(100, -3).Results())                                    //產生連續數列
+	fmt.Println(linq.From(numbers).SumInts())                                     //加總
+	fmt.Println(linq.From(numbers).Average())                                     //平均
+	fmt.Println(linq.From(numbers).Min(), linq.From(numbers).Max())               //最大最小
+	fmt.Println(linq.From(words).Last())                                          //取最後一個
+	fmt.Println(linq.From([]int{1, 2, 3, 4, 4, 4, 4, 5}).Distinct().Results()...) //去重
+	fmt.Println(linq.From([]int{1, 2, 3, 4, 5, 6, 7, 8}).Reverse().Results()...)  //反轉
 
 	//所有字母改大寫
 	var onlyUpperWords = make([]string, 0)
-	From(words).Select(func(s interface{}) interface{} {
+	linq.From(words).Select(func(s interface{}) interface{} {
 		return strings.ToUpper(s.(string))
 	}).ToSlice(&onlyUpperWords)
 	fmt.Println(onlyUpperWords)
 
 	//ForEach 印出每個單字加上間隔
-	From(words).ForEach(func(s interface{}) {
+	linq.From(words).ForEach(func(s interface{}) {
 		fmt.Print("-", s.(string))
 	})
 	fmt.Println()
 
 	//找 Index
-	fmt.Printf("Boos at %d\n", From(words).IndexOf(func(item interface{}) bool { return item == "Boss" }))
+	fmt.Printf("Boos at %d\n", linq.From(words).IndexOf(func(item interface{}) bool { return item == "Boss" }))
 
 	//挑選出含有“o”的單字
 	var wordsConatinsAlphao = make([]string, 0)
-	From(words).Where(func(s interface{}) bool {
+	linq.From(words).Where(func(s interface{}) bool {
 		return strings.Contains(s.(string), "o")
 	}).ToSlice(&wordsConatinsAlphao)
 	fmt.Println(wordsConatinsAlphao)
